d40de6acaa7bfe144d9a02da5d68c296bfc1213b
soojung.an <soojung.an@yanolja.com>
2021-01-05 09:43:37 +0900

    init commit

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //public method 에 transaction 이 기본 적용
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member){
+
+        validateDuplicateMember(member.getName());
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(String name) {
+        List<Member> findMembers = memberRepository.findByName(name);
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+
+}
